7b6083ffa186cefaf271ce41a0d1ebcdf7bd7ca3
yp583 <pately583@gmail.com>
2023-01-31 16:14:28 -0500

    added function to easily get data for any ticker across scripts

--- Week1-4/datagathering.py
+++ Week1-4/datagathering.py
@@ -1,10 +1,14 @@
 import yfinance as yf
 
-s&p500 = "^GSPC" #the S&P500 index symbol on yahoo finance. This allows me to get the market data for the top 500 companies which are highly infuential to short term price movements
-ticker = "AAPL" #the ticker of the company to be trained on
+sp500 = "^GSPC" #the S&P500 index symbol on yahoo finance. This allows me to get the market data for the top 500 companies which are highly infuential to short term price movements
+ticker_ = "AAPL" #the ticker of the company to be trained on
 
-info = yf.Ticker(ticker) #initializes the ticker with yfinance library
-marketinfo = yf.Ticker(s&p500)
+info_ = yf.Ticker(ticker_) #initializes the ticker with yfinance library
+marketinfo = yf.Ticker(sp500)
 
-hist = info.history(period="20y") #data going back 20 years. Returned is a pandas dataframe.
+hist = info_.history(period="20y") #data going back 20 years. Returned is a pandas dataframe.
 markethist = marketinfo.history(period="20y") #data going back 20 years. Returned is a pandas dataframe.
+
+def gethist(ticker, period):
+    info = yf.Ticker(ticker) #initializes the ticker with yfinance library
+    return info.history(period=period) #data going back to however many years specified by parameter. Returned is a pandas dataframe.
