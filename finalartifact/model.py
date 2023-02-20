e8a4f3333c4c9d5b208914a1ac707db5a4877ae3
yp583 <pately583@gmail.com>
2023-02-20 14:39:00 -0500

    some minor tweaks to finetune what sequence lengths for training and predicting are best for the model. Currently it seems that using the last 5 days to predict the next 5 days are good for predictions.

--- finalartifact/model.py
+++ finalartifact/model.py
@@ -19,27 +19,27 @@ class StockPredictionModel:
         return history
     def predict(self, test_X):
         forecast = self.model.predict(test_X, verbose=0)
-        return [x for x in forecast[0, :]]
+        return forecast
 
 
 #testing out classes and model
 import datagathering as dg
 import dataprep as dp
 
-raw = dg.gethist("AMZN", "max")
-data_X, data_Y = dp.prepdata(raw, 60, 30)
+raw = dg.gethist("AMZN", "20y")
+data_X, data_Y = dp.prepdata(raw, 5, 5)
 
 tdpy = 237 #237 trading days a year
 
 train_X, train_Y = data_X[:tdpy * 18, :], data_Y[:tdpy * 18, :]
-test_X, test_Y = data_X[tdpy * 18:tdpy * 19, :], data_Y[tdpy * 18:tdpy * 19, :]
+test_X, test_Y = data_X[tdpy * 19:tdpy * 20, :], data_Y[tdpy * 19:tdpy * 20, :]
 
 spm = StockPredictionModel([train_X.shape[1], train_X.shape[2]], train_Y.shape[1])
 spm.train(train_X, train_Y)
 yhat = spm.predict(test_X)
-print(yhat)
+print(yhat[:, 0])
 plt.plot(test_Y[:, 0], label="atcual")
-plt.plot(yhat, label="prediction")
+plt.plot(yhat[:, 4], label="prediction")
 
 plt.legend()
 plt.show()
