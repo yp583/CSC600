2ac483204eb49184c89f46ce683d39aa747cfda3
yp583 <pately583@gmail.com>
2023-02-27 09:28:38 -0500

    removed testing code from model.py and created a new file called test.py that contains this code.

---
+++ finalartifact/test.py
@@ -0,0 +1,27 @@
+#testing out classes and model
+import datagathering as dg
+import dataprep as dp
+import model as ml
+
+import matplotlib.pyplot as plt
+
+raw = dg.gethist("AMZN", "20y") #get amazon data from the last 20 years
+data_X, data_Y = dp.prepdata(raw, 5, 5) #prep the raw data
+
+tdpy = 237 #237 trading days a year
+
+train_X, train_Y = data_X[:tdpy * 18, :], data_Y[:tdpy * 18, :] #train the model on the last 18 years
+test_X, test_Y = data_X[tdpy * 19:tdpy * 20, :], data_Y[tdpy * 19:tdpy * 20, :] #train the model on 
+
+spm = ml.StockPredictionModel([train_X.shape[1], train_X.shape[2]], train_Y.shape[1])
+spm.train(train_X, train_Y)
+prediction = spm.predict(test_X)
+error = spm.error(test_Y, prediction)
+print(error)
+
+
+plt.plot(test_Y[:, 0], label="atcual")
+plt.plot(prediction[:, 4], label="prediction")
+
+plt.legend()
+plt.show()
